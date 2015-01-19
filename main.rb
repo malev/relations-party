0014c20150aa8231fbe71e1b1346bd72986ff78a
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-19 12:57:33 -0600

    Coreferencia

--- main.rb
+++ main.rb
@@ -1,4 +1,20 @@
 $: << '.'
+require 'boot'
+require 'active_support/all'
+require 'amatch'
 require 'models'
 
-puts Entity.pluck :text
+
+Entity.pluck(:tag).uniq.each do |tag|
+  Entity.where(tag: tag).each do |entity|
+    canonical = Canonical.where(tag: tag).detect { |canonical| canonical.valid_similarity_with(entity.lemma) }
+
+    if canonical.nil?
+      canonical = Canonical.create tag: tag, representations: {entity.lemma => entity.text}
+      canonical.entities << entity
+      canonical.save
+    else
+      canonical.add(entity)
+    end
+  end
+end
