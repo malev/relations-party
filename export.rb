b21aef79f770b269b62e18e4942198d5cf861d4c
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-19 17:04:24 -0600

    treemap exporter

--- export.rb
+++ export.rb
@@ -4,25 +4,47 @@ require 'json'
 require 'boot'
 require 'models'
 
-output = {name: 'Rick Perry Indictment', children: []}
 
-
-def generate_document(document)
-  output = {name: document.title, children: []}
-
-  entity_tags = Entity.where(document_id: document.id).pluck(:tag).uniq
-  entity_tags.each do |tag|
-    tmp = {name: tag, children: []}
-    Entity.where(document_id: document.id, tag: tag).each do |entity|
-      tmp[:children] << {name: entity.text, size: entity.mentions}
-    end
-    output[:children] << tmp
+class Dendrogram
+  def self.to_json
+    output = {name: 'Rick Perry Indictment', children: []}
+    Document.each do |document|
+      output[:children] << generate_document(document)
+   end
+   output.to_json
   end
-  output
+
+  def self.generate_document(document)
+    output = {name: document.title, size: document.content.size, children: []}
+
+    entity_tags = Entity.where(document_id: document.id).pluck(:tag).uniq
+    entity_tags.each do |tag|
+      tmp = {name: tag, children: []}
+      Entity.where(document_id: document.id, tag: tag).each do |entity|
+        tmp[:children] << {name: entity.text, size: entity.mentions}
+      end
+      output[:children] << tmp
+    end
+    output
+  end
 end
 
-Document.each do |document|
-  output[:children] << generate_document(document)
+class Treemap
+  def self.generate_document(document)
+    output = {name: document.title, children: []}
+    Entity.where(document_id: document, tag: 'Person').each do |entity|
+      output[:children] << {name: entity.text, size: entity.mentions}
+    end
+    output
+  end
+
+  def self.to_json
+    output = {name: 'Rick Perry Indictment', children: []}
+    Document.each do |document|
+      output[:children] << generate_document(document)
+   end
+   output.to_json
+  end
 end
 
-puts output.to_json
+puts Dendrogram.to_json
